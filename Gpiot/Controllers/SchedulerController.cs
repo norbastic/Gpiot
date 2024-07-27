2ac6f0b8683e791e10512f9fbf1a7dcf738be31b
norbastic <norbert.faragooo@gmail.com>
2024-07-27 23:59:48 +0200

    Dummy HttpPost SchedulerController

--- Gpiot/Controllers/SchedulerController.cs
+++ Gpiot/Controllers/SchedulerController.cs
@@ -22,11 +22,16 @@ public class SchedulerController(ISchedulerFactory schedulerFactory) : Controlle
     [HttpPost]
     public async Task<ActionResult> ScheduleJob([FromBody] GpioEventSchedule schedule)
     {
+        
         var scheduler = await _schedulerFactory.GetScheduler();
         var job = JobBuilder.Create().WithIdentity(schedule.ScheduleName);
         var trigger = TriggerBuilder.Create()
             .WithIdentity($"{schedule.ScheduleName}-trigger")
-            .WithSimpleSchedule(x => x.)
-        return StatusCode(StatusCodes.Status200OK);
+            .WithSimpleSchedule(x => x.WithIntervalInHours(2))
+            .Build();
+
+        await scheduler.ScheduleJob(job.Build(), trigger);
+
+        return StatusCode(StatusCodes.Status201Created);
     }
 }
